7b7df919baaf96f30698efd894ed2f76a32c0e1a
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-12-16 10:38:23 -0800

    Add image and string resources and built out composables without styling

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
@@ -24,7 +26,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Article("Android")
+                    Article(
+                        stringResource(R.string.jetpack_compose_tutorial),
+                        stringResource(R.string.main_p1),
+                        stringResource(R.string.main_p2))
                 }
             }
         }
@@ -32,32 +37,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Article(name: String, modifier: Modifier = Modifier) {
+fun Article(title: String, p1: String, p2: String, modifier: Modifier = Modifier) {
     Column {
-
+        val image = painterResource(id = R.drawable.bg_compose_background)
         Image(
-            painterResource(id = ),
-            
+            painter = image,
+            contentDescription = null
         )
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Text(
-            text = "Hello $name!",
+        ArticleText(
+            title,
+            p1,
+            p2,
             modifier = modifier
         )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ArticleText(title: String, p1: String, p2: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        modifier = modifier
+    )
+    Text(
+        text = p1,
+        modifier = modifier
+    )
+    Text(
+        text = p2,
+        modifier = modifier
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Article("Android")
+        Article(
+            stringResource(R.string.jetpack_compose_tutorial),
+            stringResource(R.string.main_p1),
+            stringResource(R.string.main_p2)
+            )
     }
 }
