3753f222d7ad62ec24ba10594458642cfe352f79
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-12-16 10:49:42 -0800

    Add styling to main activity

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +47,9 @@ fun Article(title: String, p1: String, p2: String, modifier: Modifier = Modifier
         val image = painterResource(id = R.drawable.bg_compose_background)
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            modifier = modifier
+                .fillMaxWidth()
         )
         ArticleText(
             title,
@@ -57,15 +64,21 @@ fun Article(title: String, p1: String, p2: String, modifier: Modifier = Modifier
 fun ArticleText(title: String, p1: String, p2: String, modifier: Modifier = Modifier) {
     Text(
         text = title,
+        fontSize = 24.sp,
         modifier = modifier
+            .padding(16.dp)
     )
     Text(
         text = p1,
+        textAlign = TextAlign.Justify,
         modifier = modifier
+            .padding(16.dp)
     )
     Text(
         text = p2,
+        textAlign = TextAlign.Justify,
         modifier = modifier
+            .padding(16.dp)
     )
 }
 
